653e1cc1d4d2fbfe01b13b6263b64fae39ad1bd0
locodurian <leon.runlongli@gmail.com>
2024-02-18 09:53:35 +0800

    UI update

--- MyTodo/ViewModels/IndexViewModel.cs
+++ MyTodo/ViewModels/IndexViewModel.cs
@@ -1,12 +1,78 @@
-﻿using System;
+﻿using MyTodo.Common.Models;
+using Prism.Mvvm;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MyTodo.ViewModels
 {
-    public class IndexViewModel
+    public class IndexViewModel : BindableBase
     {
+        public IndexViewModel()
+        {
+            TaskBars = new ObservableCollection<TaskBar>();
+            CreateTaskBars();
+            CreateTestData();
+        }
+
+        public void CreateTaskBars()
+        {
+            TaskBars.Add(new TaskBar { Icon = "ClockFast", Title = "汇总", Content = "9", Color = "#ff0ca0ff", Target = "" });
+            TaskBars.Add(new TaskBar { Icon = "ClockCheckOutline", Title = "已完成", Content = "9", Color = "#ff1eca3a", Target = "" });
+            TaskBars.Add(new TaskBar { Icon = "ChartLineVariant", Title = "完成比例", Content = "100%", Color = "#ff02c6dc", Target = "" });
+            TaskBars.Add(new TaskBar { Icon = "PlaylistStar", Title = "备忘录", Content = "19", Color = "#ffffa000", Target = "" });
+        }
+        public void CreateTestData()
+        {
+            ToDoDtos = new ObservableCollection<ToDoDto>();
+            MemoDtos = new ObservableCollection<MemoDto>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                ToDoDtos.Add(new ToDoDto { Title = "待办" + i, Content = "正在处理中..." });
+                MemoDtos.Add(new MemoDto { Title = "备忘" + i, Content = "我的密码" });
+            }
+        }
+
+        private ObservableCollection<TaskBar> _taskBars;
+
+        public ObservableCollection<TaskBar> TaskBars
+        {
+            get { return _taskBars; }
+            set
+            {
+                _taskBars = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private ObservableCollection<ToDoDto> _toDoDtos;
+
+        public ObservableCollection<ToDoDto> ToDoDtos
+        {
+            get { return _toDoDtos; }
+            set
+            {
+                _toDoDtos = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private ObservableCollection<MemoDto> _memoDto;
+
+        public ObservableCollection<MemoDto> MemoDtos
+        {
+            get { return _memoDto; }
+            set
+            {
+                _memoDto = value;
+                RaisePropertyChanged();
+            }
+        }
+
     }
+
 }
--- MyTodo/ViewModels/ToDoViewModel.cs
+++ MyTodo/ViewModels/ToDoViewModel.cs
@@ -1,12 +1,63 @@
-﻿using System;
+﻿using MyTodo.Common.Models;
+using Prism.Commands;
+using Prism.Mvvm;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MyTodo.ViewModels
 {
-    public class ToDoViewModel
+    public class ToDoViewModel : BindableBase
     {
+        public ToDoViewModel()
+        {
+            ToDoDtos = new ObservableCollection<ToDoDto>();
+            CreateToDoList();
+            AddCommand = new DelegateCommand(Add);
+        }
+
+        private void Add()
+        {
+            IsRightDrawerOpen = !IsRightDrawerOpen;
+        }
+
+        public void CreateToDoList()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                ToDoDtos.Add(
+                    new ToDoDto()
+                    {
+                        Title = "标题" + i,
+                        Content = "测试数据" + i
+                    }
+                    );
+            }
+        }
+
+        public DelegateCommand AddCommand { get; private set; }
+
+
+        private ObservableCollection<ToDoDto> _toDoDtos;
+        private bool _isRightDrawerOpen;
+
+        public bool IsRightDrawerOpen
+        {
+            get { return _isRightDrawerOpen; }
+            set
+            {
+                _isRightDrawerOpen = value;
+                RaisePropertyChanged();
+            }
+        }
+        public ObservableCollection<ToDoDto> ToDoDtos
+        {
+            get { return _toDoDtos; }
+            set { _toDoDtos = value; }
+        }
+
     }
 }
